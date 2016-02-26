c54afca33c47ee30a57fa6d23ca4913d083442b6
nrainer <nrainer@users.noreply.github.com>
2016-02-26 15:42:03 +0100

    CodeStatisticsWorkflow: computation can be configured

--- test-analyzer-extensions/src/main/java/de/tum/in/niedermr/ta/extensions/analysis/workflows/statistics/CodeStatisticsWorkflow.java
+++ test-analyzer-extensions/src/main/java/de/tum/in/niedermr/ta/extensions/analysis/workflows/statistics/CodeStatisticsWorkflow.java
@@ -4,21 +4,31 @@ import de.tum.in.niedermr.ta.extensions.analysis.workflows.statistics.steps.Asse
 import de.tum.in.niedermr.ta.extensions.analysis.workflows.statistics.steps.InstructionCounterStep;
 import de.tum.in.niedermr.ta.extensions.analysis.workflows.statistics.steps.PersistResultStep;
 import de.tum.in.niedermr.ta.runner.analysis.workflow.AbstractWorkflow;
+import de.tum.in.niedermr.ta.runner.configuration.extension.ConfigurationExtensionKey;
 import de.tum.in.niedermr.ta.runner.execution.exceptions.FailedExecution;
 
 public class CodeStatisticsWorkflow extends AbstractWorkflow {
+	public static final ConfigurationExtensionKey COUNT_INSTRUCTIONS = ConfigurationExtensionKey
+			.create("code.statistics.method.instructions");
+	public static final ConfigurationExtensionKey COUNT_ASSERTIONS = ConfigurationExtensionKey
+			.create("code.statistics.method.assertions");
+
 	@Override
 	public void start() throws FailedExecution {
-		InstructionCounterStep countInstructionsStep = new InstructionCounterStep(super.m_information);
-		AssertionCounterStep countAssertionsStep = new AssertionCounterStep(super.m_information);
 		PersistResultStep persistResultStep = new PersistResultStep(super.m_information);
 
-		countInstructionsStep.run();
-		countAssertionsStep.run();
+		if (m_information.getConfiguration().getConfigurationExtension().getBooleanValue(COUNT_INSTRUCTIONS)) {
+			InstructionCounterStep countInstructionsStep = new InstructionCounterStep(super.m_information);
+			countInstructionsStep.run();
+			persistResultStep.addResultInstructionsPerMethod(countInstructionsStep.getInstructionsPerMethod());
+			persistResultStep.addResultInstructionsPerTestcase(countInstructionsStep.getInstructionsPerTestcase());
+		}
 
-		persistResultStep.addResultInstructionsPerMethod(countInstructionsStep.getInstructionsPerMethod());
-		persistResultStep.addResultInstructionsPerTestcase(countInstructionsStep.getInstructionsPerTestcase());
-		persistResultStep.addResultAssertionsPerTestcase(countAssertionsStep.getAssertionsPerTestcase());
+		if (m_information.getConfiguration().getConfigurationExtension().getBooleanValue(COUNT_ASSERTIONS)) {
+			AssertionCounterStep countAssertionsStep = new AssertionCounterStep(super.m_information);
+			countAssertionsStep.run();
+			persistResultStep.addResultAssertionsPerTestcase(countAssertionsStep.getAssertionsPerTestcase());
+		}
 
 		persistResultStep.run();
 	}
