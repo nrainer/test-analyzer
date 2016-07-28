29001b4ae54787749b744fb8e91befda0941d235
nrainer <nrainer@users.noreply.github.com>
2016-07-28 12:01:17 +0200

    Added sql_import.bat

---
+++ test-analyzer-runner/sql/sql_import.bat
@@ -0,0 +1,5 @@
+mysql testanalyzer < ./execution-information.sql.txt
+mysql testanalyzer < ./collected-information.sql.txt
+mysql testanalyzer < ./result.sql.txt
+mysql testanalyzer < ./analysis-information.sql.txt
+mysql testanalyzer < ./code-statistics.sql.txt
