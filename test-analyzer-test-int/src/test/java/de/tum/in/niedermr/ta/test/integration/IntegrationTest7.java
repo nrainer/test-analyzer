67d5afa021707ca82b3d81b2dcf4e400b17d9e7a
nrainer <nrainer@users.noreply.github.com>
2016-07-29 11:23:12 +0200

    Extended integration test 7: check code statistics output

--- test-analyzer-test-int/src/test/java/de/tum/in/niedermr/ta/test/integration/IntegrationTest7.java
+++ test-analyzer-test-int/src/test/java/de/tum/in/niedermr/ta/test/integration/IntegrationTest7.java
@@ -7,24 +7,31 @@ import de.tum.in.niedermr.ta.runner.configuration.exceptions.ConfigurationExcept
 import de.tum.in.niedermr.ta.runner.execution.environment.EnvironmentConstants;
 
 /**
- * Jar: simple-project-core.jar Multi-threaded: no Comments: stack analysis
- * workflow
+ * Jar: simple-project-core.jar Multi-threaded: no Comments: stack analysis workflow
  */
 public class IntegrationTest7 extends AbstractSystemTest {
 	private static final String ANALYSIS_INFORMATION_OUTPUT = EnvironmentConstants.PATH_WORKING_AREA_RESULT
 			+ "analysis-information" + FILE_EXTENSION_SQL_TXT;
+	private static final String CODE_STATISTICS_OUTPUT = EnvironmentConstants.PATH_WORKING_AREA_RESULT
+			+ "code-statistics" + FILE_EXTENSION_SQL_TXT;
 
 	@Override
 	public void testSystemInternal() throws ConfigurationException, IOException {
 		File expectedStackAnalysisFile = getExpectedFile(getFileName(ANALYSIS_INFORMATION_OUTPUT));
 		File outputStackAnalysisFile = getOutputFile(getFileName(ANALYSIS_INFORMATION_OUTPUT));
+		File expectedCodeStatisticsFile = getExpectedFile(getFileName(CODE_STATISTICS_OUTPUT));
+		File outputCodeStatisticsFile = getOutputFile(getFileName(CODE_STATISTICS_OUTPUT));
 
 		assertFileExists(MSG_PATH_TO_TEST_JAR_IS_INCORRECT, new File(getCommonFolderTestData() + JAR_TEST_DATA));
 		assertFileExists(MSG_TEST_DATA_MISSING, expectedStackAnalysisFile);
+		assertFileExists(MSG_TEST_DATA_MISSING, expectedCodeStatisticsFile);
 
 		executeTestAnalyzerWithConfiguration();
 
 		assertFileExists(MSG_OUTPUT_MISSING, outputStackAnalysisFile);
 		assertFileContentEqual(MSG_NOT_EQUAL_RESULT, false, expectedStackAnalysisFile, outputStackAnalysisFile);
+
+		assertFileExists(MSG_OUTPUT_MISSING, outputCodeStatisticsFile);
+		assertFileContentEqual(MSG_NOT_EQUAL_RESULT, false, expectedCodeStatisticsFile, outputCodeStatisticsFile);
 	}
 }
