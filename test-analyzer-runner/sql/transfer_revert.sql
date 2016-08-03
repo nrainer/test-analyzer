094805f6f4e10ea445be862aba592bd5edfe1b0b
nrainer <nrainer@users.noreply.github.com>
2016-08-03 10:49:01 +0200

    SQL schema: revert import

---
+++ test-analyzer-runner/sql/transfer_revert.sql
@@ -0,0 +1,39 @@
+SET @executionId = '';
+
+START TRANSACTION;
+
+DELETE FROM Method_Info
+WHERE execution = @executionId;
+
+DELETE FROM Testcase_Info
+WHERE execution = @executionId;
+
+DELETE FROM Relation_Info
+WHERE execution = @executionId;
+
+DELETE FROM Test_Result_Info
+WHERE execution = @executionId;
+
+DELETE FROM Method_Test_Abort_Info
+WHERE execution = @executionId;
+
+/* Mark all entries in Collected_Information_Import as not processed. */
+UPDATE Collected_Information_Import c
+SET c.processed = 0
+WHERE c.execution = @executionId;
+
+/* Mark all entries in Test_Result_Import as not processed. */
+UPDATE Test_Result_Import t
+SET t.processed = 0
+WHERE t.execution = @executionId;
+
+/* Mark all entries in Test_Abort_Import as not processed. */
+UPDATE Test_Abort_Import t
+SET t.processed = 0
+WHERE t.execution = @executionId;
+
+UPDATE Execution_Information
+SET importProcessed = 0
+WHERE execution = @executionId;
+
+COMMIT;
