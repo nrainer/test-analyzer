a66b608274407f76bdfc3e7c2233029a26158eed
nrainer <nrainer@users.noreply.github.com>
2016-08-26 16:00:16 +0200

    SQL schema: extended analysis schema

--- test-analyzer-runner/sql/schema/analysis_schema.sql
+++ test-analyzer-runner/sql/schema/analysis_schema.sql
@@ -1,9 +1,10 @@
--- DROP TABLE IF EXISTS Method_Info;
--- DROP TABLE IF EXISTS Testcase_Info;
--- DROP TABLE IF EXISTS Relation_Info;
--- DROP TABLE IF EXISTS RetValGen_Info;
--- DROP TABLE IF EXISTS Test_Result_Info;
+-- DROP TABLE IF EXISTS Method_Classification_Info;
 -- DROP TABLE IF EXISTS Method_Test_Abort_Info;
+-- DROP TABLE IF EXISTS Test_Result_Info;
+-- DROP TABLE IF EXISTS RetValGen_Info;
+-- DROP TABLE IF EXISTS Relation_Info;
+-- DROP TABLE IF EXISTS Testcase_Info;
+-- DROP TABLE IF EXISTS Method_Info;
 
 CREATE TABLE Method_Info
 (
@@ -12,6 +13,7 @@ CREATE TABLE Method_Info
 	method VARCHAR(1024) NOT NULL COLLATE UTF8_BIN,
     instructions INT(8),
     modifier VARCHAR(10),
+    classificationId INT REFERENCES Method_Classification_Info(classificationId),
 	methodHash VARCHAR(32) GENERATED ALWAYS AS (MD5(method)) VIRTUAL
 );
 
@@ -62,7 +64,18 @@ CREATE TABLE Method_Test_Abort_Info
 	retValGenId INT(11) NOT NULL REFERENCES RetValGen_Info(retValGenId)
 );
 
+CREATE TABLE Method_Classification_Info
+(
+  classificationId int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(64) NOT NULL,
+  category VARCHAR(64) NOT NULL,
+  description VARCHAR(64),
+  example VARCHAR(64),
+  severity VARCHAR(12)
+);
+
 CREATE INDEX idx_aly_mi_1 ON Method_Info(execution, methodHash);
+CREATE INDEX idx_aly_mi_2 ON Method_Info(classificationId);
 CREATE INDEX idx_aly_ti_1 ON Testcase_Info(execution, testcaseHash);
 CREATE INDEX idx_aly_ri_1 ON Relation_Info(execution, methodId, testcaseId);
 CREATE INDEX idx_aly_ri_2 ON Relation_Info(testcaseId);
@@ -73,3 +86,4 @@ CREATE INDEX idx_aly_tri_2 ON Test_Result_Info(testcaseId);
 ALTER TABLE Relation_Info ADD CONSTRAINT uc_aly_ri_1 UNIQUE (execution, methodId, testcaseId);
 ALTER TABLE Test_Result_Info ADD CONSTRAINT uc_aly_tri_1 UNIQUE (execution, methodId, testcaseId, retValGenId);
 ALTER TABLE Method_Test_Abort_Info ADD CONSTRAINT uc_aly_mai_1 UNIQUE (execution, methodId, retValGenId);
+ALTER TABLE Method_Classification_Info ADD CONSTRAINT uc_aly_mci_1 UNIQUE (name);
