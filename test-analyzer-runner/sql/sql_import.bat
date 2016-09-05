71d2db755a9fb43a0118fc233ba465d2f58a38f8
nrainer <nrainer@users.noreply.github.com>
2016-09-05 14:09:08 +0200

    SQL schema: restructured files

--- test-analyzer-runner/sql/sql_import.bat
+++
@@ -1,5 +0,0 @@
-mysql testanalyzer < ./execution-information.sql.txt
-mysql testanalyzer < ./collected-information.sql.txt
-mysql testanalyzer < ./result.sql.txt
-mysql testanalyzer < ./analysis-information.sql.txt
-mysql testanalyzer < ./code-statistics.sql.txt
