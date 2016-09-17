d051c0241a358642cd77db66517b1f11c59b96c5
nrainer <nrainer@users.noreply.github.com>
2016-09-18 01:47:23 +0200

    minor

--- test-analyzer-runner/src/main/java/de/tum/in/niedermr/ta/runner/configuration/exceptions/ConfigurationException.java
+++ test-analyzer-runner/src/main/java/de/tum/in/niedermr/ta/runner/configuration/exceptions/ConfigurationException.java
@@ -2,21 +2,26 @@ package de.tum.in.niedermr.ta.runner.configuration.exceptions;
 
 import de.tum.in.niedermr.ta.runner.configuration.property.templates.IConfigurationProperty;
 
+/** Configuration exception. */
 public class ConfigurationException extends Exception {
-	private static final long serialVersionUID = 7514093845938362369L;
+	private static final long serialVersionUID = 1L;
 
+	/** Constructor. */
 	public ConfigurationException(Throwable t) {
 		super(t);
 	}
 
+	/** Constructor. */
 	public ConfigurationException(String msg) {
-		this("", msg);
+		super(msg);
 	}
 
+	/** Constructor. */
 	public ConfigurationException(IConfigurationProperty<?> property, String msg) {
 		this("At '" + property.getName() + "': ", msg);
 	}
 
+	/** Constructor. */
 	private ConfigurationException(String prefix, String msg) {
 		super(prefix + msg);
 	}
