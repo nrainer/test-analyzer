99ebaac53a6cde425ddf2c95492bd197cb340e66
nrainer <nrainer@users.noreply.github.com>
2016-09-17 18:48:19 +0200

    minor

--- test-analyzer-runner/src/main/java/de/tum/in/niedermr/ta/runner/factory/IRequiresFactoryCreation.java
+++ test-analyzer-runner/src/main/java/de/tum/in/niedermr/ta/runner/factory/IRequiresFactoryCreation.java
@@ -1,9 +1,8 @@
 package de.tum.in.niedermr.ta.runner.factory;
 
 /**
- * Marker interface to indicate that a certain class needs to be created using
- * {@link IFactory}.
+ * Marker interface to indicate that a certain class needs to be created using {@link IFactory}.
  */
 public interface IRequiresFactoryCreation {
-
+	// NOP
 }
