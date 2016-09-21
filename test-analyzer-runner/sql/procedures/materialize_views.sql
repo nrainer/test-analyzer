6b34a017676c823587c7a223abbc9f9d2a816d4b
nrainer <nrainer@users.noreply.github.com>
2016-09-21 10:25:57 +0200

    SQL schema: added views

--- test-analyzer-runner/sql/procedures/materialize_views.sql
+++ test-analyzer-runner/sql/procedures/materialize_views.sql
@@ -1,19 +1,48 @@
 DROP PROCEDURE IF EXISTS MaterializeViews;
+DROP FUNCTION IF EXISTS ProjectDisplayText;
+
+CREATE FUNCTION ProjectDisplayText(execution VARCHAR(5), project VARCHAR(64), testType VARCHAR(64))
+	RETURNS CHAR(160) DETERMINISTIC
+	RETURN CONCAT(CONCAT(CONCAT(CONCAT(CONCAT(project, ' ('), testType),'; '), execution), ')');
 
 DELIMITER //
 CREATE PROCEDURE MaterializeViews ()
 BEGIN
 
-DROP TABLE IF EXISTS MV_Tested_Methods_Info;
+DROP TABLE IF EXISTS MV_Project_Results;
+DROP TABLE IF EXISTS MV_Project_Overview;
 DROP TABLE IF EXISTS MV_Tested_Methods_Info_Agg;
+DROP TABLE IF EXISTS MV_Tested_Methods_Info;
 
 CREATE TABLE MV_Tested_Methods_Info AS SELECT * FROM V_Tested_Methods_Info;
 CREATE TABLE MV_Tested_Methods_Info_Agg AS SELECT * FROM V_Tested_Methods_Info_Agg;
+CREATE TABLE MV_Project_Overview AS SELECT * FROM V_Project_Overview;
 
 CREATE INDEX mv_tmi_1 ON MV_Tested_Methods_Info(execution);
 CREATE INDEX mv_tmi_2 ON MV_Tested_Methods_Info(methodId);
 CREATE INDEX mv_tmia_1 ON MV_Tested_Methods_Info_Agg(execution);
 CREATE INDEX mv_tmia_2 ON MV_Tested_Methods_Info_Agg(methodId);
+
+CREATE TABLE MV_Project_Results AS
+	SELECT
+		e.execution,
+		e.project,
+		e.testType,
+		e.processed,
+		e.valid,
+		(SELECT COUNT(*) FROM MV_Tested_Methods_Info_Agg vtmia WHERE vtmia.execution = e.execution AND vtmia.killedResult = 1) AS countKilledMethods,
+		(SELECT COUNT(*) FROM MV_Tested_Methods_Info_Agg vtmia WHERE vtmia.execution = e.execution AND vtmia.killedResult = 0) AS countPseudoTestedMethods,
+		(SELECT SUM(CASE WHEN vtmia.killedResult = 1 THEN 1 ELSE 0 END) / COUNT(*) FROM MV_Tested_Methods_Info_Agg vtmia WHERE vtmia.execution = e.execution) AS mutationScore,
+		(SELECT COUNT(*) FROM MV_Tested_Methods_Info_Agg vtmia WHERE vtmia.execution = e.execution AND vtmia.killedResult = 1 AND vtmia.method NOT LIKE '%hashCode()') AS countKilledMethodsFiltered,
+		(SELECT COUNT(*) FROM MV_Tested_Methods_Info_Agg vtmia WHERE vtmia.execution = e.execution AND vtmia.killedResult = 0 AND vtmia.method NOT LIKE '%hashCode()') AS countPseudoTestedMethodsFiltered,
+		(SELECT SUM(CASE WHEN vtmia.killedResult = 1 THEN 1 ELSE 0 END) / COUNT(*) FROM MV_Tested_Methods_Info_Agg vtmia WHERE vtmia.execution = e.execution AND vtmia.method NOT LIKE '%hashCode()') AS mutationScoreFiltered,
+		(SELECT SUM(CASE WHEN vtmia.killedResult = 1 THEN 1 ELSE 0 END) / COUNT(*) FROM MV_Tested_Methods_Info_Agg vtmia WHERE vtmia.execution = e.execution AND vtmia.method NOT LIKE '%hashCode()' AND vtmia.minStackDistance >= 5) AS mutationScoreFilteredD5,
+		(SELECT SUM(CASE WHEN vtmia.killedResult = 1 THEN 1 ELSE 0 END) / COUNT(*) FROM MV_Tested_Methods_Info_Agg vtmia WHERE vtmia.execution = e.execution AND vtmia.method NOT LIKE '%hashCode()' AND vtmia.minStackDistance >= 6) AS mutationScoreFilteredD6,
+		(SELECT SUM(CASE WHEN vtmia.killedResult = 1 THEN 1 ELSE 0 END) / COUNT(*) FROM MV_Tested_Methods_Info_Agg vtmia WHERE vtmia.execution = e.execution AND vtmia.method NOT LIKE '%hashCode()' AND vtmia.minStackDistance >= 7) AS mutationScoreFilteredD7,
+		(SELECT AVG(vtmia.minStackDistance) FROM MV_Tested_Methods_Info_Agg vtmia WHERE vtmia.execution = e.execution AND vtmia.killedResult = 1) AS avgMinStackDistKilled,
+		(SELECT AVG(vtmia.minStackDistance) FROM MV_Tested_Methods_Info_Agg vtmia WHERE vtmia.execution = e.execution AND vtmia.killedResult = 0) AS avgMinStackDistLiving
+	FROM Execution_Information e
+	ORDER BY e.testType, e.project;
 
 END //
 DELIMITER ;
--- test-analyzer-runner/sql/schema/3_analysis_views.sql
+++ test-analyzer-runner/sql/schema/3_analysis_views.sql
@@ -1,3 +1,5 @@
+-- DROP VIEW IF EXISTS V_Project_Overview;
+-- DROP VIEW IF EXISTS V_Project_Overview_Sub;
 -- DROP VIEW IF EXISTS V_Method_Classification;
 -- DROP VIEW IF EXISTS V_Test_Result_Info;
 -- DROP VIEW IF EXISTS V_Tested_Methods_Info_Agg;
@@ -137,3 +139,84 @@ CREATE VIEW V_Method_Classification
 	AND vtmia.methodId = mi.methodId
 	LEFT OUTER JOIN Method_Classification_Info mci
 	ON mi.classificationId = mci.classificationId;
+	
+CREATE VIEW V_Project_Overview_Sub
+(
+	execution,
+	countCollectedMethods,
+	countCollectedTestcases,
+	countRelations,
+	countReturnValueGenerators,
+	countExecutedTestcases,
+	countTestExecutedMethods,
+	countExecutedRelations,
+	countExecutedRelationRuns,
+	countAbortedRelationRuns
+) AS
+	SELECT
+		e.execution,
+		-- number of methods executed by at least one successful test case
+		(SELECT COUNT(*) FROM Method_Info mi WHERE mi.execution = e.execution) AS countCollectedMethods,
+		-- number of successful test cases
+		(SELECT COUNT(*) FROM Testcase_Info ti WHERE ti.execution = e.execution) AS countCollectedTestcases,
+		-- number of pairs of successful test cases and their executed methods
+		(SELECT COUNT(*) FROM Relation_Info ri WHERE ri.execution = e.execution) AS countRelations,
+		-- number of used return value generators
+		(SELECT COUNT(*) FROM RetValGen_Info rvg WHERE rvg.execution = e.execution) AS countReturnValueGenerators,
+		-- number of test cases that were executed because of at least one covered method that was mutated
+		(SELECT COUNT(DISTINCT tri.testcaseId) FROM Test_Result_Info tri WHERE tri.execution = e.execution) AS countExecutedTestcases,
+		-- number of methods that were mutated and tested (without test abort)
+		(SELECT COUNT(DISTINCT tri.methodId) FROM Test_Result_Info tri WHERE tri.execution = e.execution) AS countTestExecutedMethods,
+		-- number of test executed pairs of test cases and methods
+		(SELECT COUNT(DISTINCT tri.testcaseId, tri.methodId) FROM Test_Result_Info tri WHERE tri.execution = e.execution) AS countExecutedRelations,
+		-- number of test executed test runs for pairs of test cases and methods (some pairs are tested with multiple return value generators
+		(SELECT COUNT(*) FROM Test_Result_Info tri WHERE tri.execution = e.execution) AS countExecutedRelationRuns,
+		-- number of methods that caused a test abort (eg: timeout)
+		(SELECT COUNT(*) FROM Method_Test_Abort_Info mtai WHERE mtai.execution = e.execution) AS countAbortedRelationRuns
+	FROM Execution_Information e;
+	
+CREATE VIEW V_Project_Overview AS
+	SELECT
+		e.execution,
+		e.project,
+		e.testType,
+		e.processed,
+		e.valid,
+		pos.countCollectedMethods,
+		pos.countCollectedTestcases,
+		pos.countRelations,
+		pos.countReturnValueGenerators,
+		pos.countExecutedTestcases,
+		pos.countTestExecutedMethods,
+		pos.countExecutedRelations,
+		pos.countExecutedRelationRuns,
+		pos.countAbortedRelationRuns,
+		-- ratio out of covered methods
+		(pos.countTestExecutedMethods / pos.countCollectedMethods) AS ratioExecutedOfCoveredMethods,
+		-- ratio out of successful test cases
+		(pos.countExecutedTestcases / pos.countCollectedTestcases) AS ratioExecutedTestcases,
+		(pos.countExecutedRelations / pos.countRelations) AS ratioExecutedRelations,
+		(pos.countExecutedRelationRuns / pos.countTestExecutedMethods) AS avgTestPerTestExecutedMethod,
+		(SELECT AVG(ti.instructions) FROM Testcase_Info ti WHERE ti.execution = e.execution) AS avgTestcaseInstructions,
+		(SELECT AVG(ti.assertions) FROM Testcase_Info ti WHERE ti.execution = e.execution) AS avgTestcaseAssertions,
+		(SELECT AVG(mi.bytecodeInstructionCount) FROM Method_Info mi WHERE mi.execution = e.execution) AS avgMethodBytecodeInstructions,
+		(SELECT AVG(mi.instructionCount) FROM Method_Info mi WHERE mi.execution = e.execution) AS avgMethodInstructionCount,
+		(SELECT AVG(mi.branchCoverage) FROM Method_Info mi WHERE mi.execution = e.execution) AS avgMethodBranchCoverage,
+		(SELECT AVG(ri.minStackDistance) FROM Relation_Info ri WHERE ri.execution = e.execution AND (ri.methodId, ri.testcaseId) IN (SELECT r.methodId, r.testcaseId FROM Test_Result_Info r WHERE r.execution = e.execution)) AS avgMinStackDistOfExecuted,
+		(SELECT STDDEV(ri.minStackDistance) FROM Relation_Info ri WHERE ri.execution = e.execution AND (ri.methodId, ri.testcaseId) IN (SELECT r.methodId, r.testcaseId FROM Test_Result_Info r WHERE r.execution = e.execution)) AS stddevMinStackDistOfExecuted,
+		(SELECT MAX(ri.minStackDistance) FROM Relation_Info ri WHERE ri.execution = e.execution AND (ri.methodId, ri.testcaseId) IN (SELECT r.methodId, r.testcaseId FROM Test_Result_Info r WHERE r.execution = e.execution)) AS absMaxOfMinStackDistOfExecuted,
+		(SELECT COUNT(*) > 0 FROM Testcase_Info ti WHERE ti.execution = e.execution AND ti.instructions IS NOT NULL AND ti.instructions IS NOT NULL) AS hasTestcaseInformation,
+		(SELECT COUNT(*) > 0 FROM Method_Info mi WHERE mi.execution = e.execution AND mi.bytecodeInstructionCount IS NOT NULL AND mi.modifier IS NOT NULL) AS hasMethodInformation,
+		(SELECT COUNT(*) > 0 FROM Method_Info mi WHERE mi.execution = e.execution AND mi.instructionCovered IS NOT NULL AND mi.branchCovered IS NOT NULL) AS hasCoverageInformation,
+		(SELECT COUNT(*) > 0 FROM Relation_Info ri WHERE ri.execution = e.execution AND ri.minStackDistance IS NOT NULL) AS hasStackDistanceInformation,
+		(SELECT COUNT(*) > 0 FROM Method_Info mi WHERE mi.execution = e.execution AND mi.classificationId IS NOT NULL) AS areMethodsClassified,
+		e.description,
+		e.notes,
+		e.methodCoverage AS projectMethodCoverage,
+		e.lineCoverage AS projectLineCoverage,
+		e.instructionCoverage AS projectInstructionCoverage,
+		e.branchCoverage AS projectBranchCoverage
+	FROM Execution_Information e
+	INNER JOIN V_Project_Overview_Sub pos
+	ON e.execution = pos.execution
+	ORDER BY e.testType, e.project;
