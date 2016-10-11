9314af7df842853a5a748fabddc2a8ca271305b1
nrainer <nrainer@users.noreply.github.com>
2016-10-11 11:23:53 +0200

    SQL procedures: extracted code

--- test-analyzer-runner/sql/procedures/materialize_views.sql
+++ test-analyzer-runner/sql/procedures/materialize_views.sql
@@ -1,9 +1,4 @@
 DROP PROCEDURE IF EXISTS MaterializeViews;
-DROP FUNCTION IF EXISTS ProjectDisplayText;
-
-CREATE FUNCTION ProjectDisplayText(execution VARCHAR(5), project VARCHAR(64), testType VARCHAR(64))
-	RETURNS CHAR(160) DETERMINISTIC
-	RETURN CONCAT(CONCAT(CONCAT(CONCAT(CONCAT(project, ' ('), testType),'; '), execution), ')');
 
 DELIMITER //
 CREATE PROCEDURE MaterializeViews ()
