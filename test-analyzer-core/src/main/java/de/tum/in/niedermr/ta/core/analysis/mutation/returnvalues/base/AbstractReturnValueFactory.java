8d36d302d54d5b9f952d1d37628bd9aa07168181
nrainer <nrainer@users.noreply.github.com>
2016-12-31 14:21:25 +0100

    Allow return value factories specifying a fallback factory

--- test-analyzer-core/src/main/java/de/tum/in/niedermr/ta/core/analysis/mutation/returnvalues/base/AbstractReturnValueFactory.java
+++ test-analyzer-core/src/main/java/de/tum/in/niedermr/ta/core/analysis/mutation/returnvalues/base/AbstractReturnValueFactory.java
@@ -1,16 +1,23 @@
 package de.tum.in.niedermr.ta.core.analysis.mutation.returnvalues.base;
 
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import de.tum.in.niedermr.ta.core.code.identifier.MethodIdentifier;
 
 public abstract class AbstractReturnValueFactory implements IReturnValueFactory {
 
+	private Optional<AbstractReturnValueFactory> m_fallbackFactory;
+
+	public AbstractReturnValueFactory() {
+		m_fallbackFactory = getConfiguredFallbackFactory();
+	}
+
 	/** {@inheritDoc} */
 	@Override
 	public boolean supports(MethodIdentifier methodIdentifier, String returnType) {
 		try {
-			getWithException(methodIdentifier, returnType);
+			getRecursiveWithException(methodIdentifier, returnType);
 			return true;
 		} catch (NoSuchElementException ex) {
 			return false;
@@ -23,9 +30,30 @@ public abstract class AbstractReturnValueFactory implements IReturnValueFactory 
 	@Override
 	public final Object get(String identifierAsString, String returnType) {
 		try {
-			return getWithException(MethodIdentifier.parse(identifierAsString), returnType);
+			return getRecursiveWithException(MethodIdentifier.parse(identifierAsString), returnType);
 		} catch (NoSuchElementException ex) {
 			return null;
 		}
 	}
+
+	protected Object getRecursiveWithException(MethodIdentifier methodIdentifier, String returnType)
+			throws NoSuchElementException {
+		try {
+			return getWithException(methodIdentifier, returnType);
+		} catch (NoSuchElementException e) {
+			if (m_fallbackFactory.isPresent()) {
+				return m_fallbackFactory.get().getWithException(methodIdentifier, returnType);
+			}
+
+			throw e;
+		}
+	}
+
+	/** Get a fallback factory in case this factory does not support the given type. */
+	protected Optional<AbstractReturnValueFactory> getConfiguredFallbackFactory() {
+		return Optional.empty();
+	}
+
+	protected abstract Object getWithException(MethodIdentifier methodIdentifier, String returnType)
+			throws NoSuchElementException;
 }
--- test-analyzer-core/src/main/java/de/tum/in/niedermr/ta/core/analysis/mutation/returnvalues/base/IReturnValueFactory.java
+++ test-analyzer-core/src/main/java/de/tum/in/niedermr/ta/core/analysis/mutation/returnvalues/base/IReturnValueFactory.java
@@ -1,7 +1,5 @@
 package de.tum.in.niedermr.ta.core.analysis.mutation.returnvalues.base;
 
-import java.util.NoSuchElementException;
-
 import de.tum.in.niedermr.ta.core.code.identifier.MethodIdentifier;
 
 /** Factory to create return value instances. */
@@ -10,6 +8,4 @@ public interface IReturnValueFactory {
 	boolean supports(MethodIdentifier methodIdentifier, String returnType);
 
 	Object get(String identifierAsString, String returnType);
-
-	Object getWithException(MethodIdentifier methodIdentifier, String returnType) throws NoSuchElementException;
 }
