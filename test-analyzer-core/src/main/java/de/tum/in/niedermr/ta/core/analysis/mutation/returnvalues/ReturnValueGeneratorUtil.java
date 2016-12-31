6eedcd88e4bbeef1dcdd1d9117aaa26148ddf3fd
nrainer <nrainer@users.noreply.github.com>
2016-12-31 13:41:53 +0100

    Remove unused method

--- test-analyzer-core/src/main/java/de/tum/in/niedermr/ta/core/analysis/mutation/returnvalues/ReturnValueGeneratorUtil.java
+++ test-analyzer-core/src/main/java/de/tum/in/niedermr/ta/core/analysis/mutation/returnvalues/ReturnValueGeneratorUtil.java
@@ -4,16 +4,15 @@ import org.objectweb.asm.Type;
 
 import de.tum.in.niedermr.ta.core.code.identifier.MethodIdentifier;
 
+/** Return value generator utils. */
 public class ReturnValueGeneratorUtil {
 
+	/** Constructor. */
 	private ReturnValueGeneratorUtil() {
 	}
 
-	public static boolean canHandleType(IReturnValueGenerator returnValueGenerator, String className, String methodName, String desc) {
-		return canHandleType(returnValueGenerator, MethodIdentifier.create(className, methodName, desc), desc);
-	}
-
-	public static boolean canHandleType(IReturnValueGenerator returnValueGenerator, MethodIdentifier methodIdentifier, String desc) {
+	public static boolean canHandleType(IReturnValueGenerator returnValueGenerator, MethodIdentifier methodIdentifier,
+			String desc) {
 		return returnValueGenerator.checkReturnValueSupported(methodIdentifier, Type.getReturnType(desc));
 	}
 }
