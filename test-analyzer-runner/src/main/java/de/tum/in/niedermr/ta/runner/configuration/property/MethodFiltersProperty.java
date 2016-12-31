727a5eaf37fac6c29c51224902b99ac482d0d3dd
nrainer <nrainer@users.noreply.github.com>
2016-12-31 13:59:40 +0100

    Setter-getter filter is not longer activated by default

--- test-analyzer-runner/src/main/java/de/tum/in/niedermr/ta/runner/configuration/property/MethodFiltersProperty.java
+++ test-analyzer-runner/src/main/java/de/tum/in/niedermr/ta/runner/configuration/property/MethodFiltersProperty.java
@@ -1,26 +1,23 @@
 package de.tum.in.niedermr.ta.runner.configuration.property;
 
 import de.tum.in.niedermr.ta.core.analysis.filter.IMethodFilter;
-import de.tum.in.niedermr.ta.core.analysis.filter.advanced.SetterGetterFilter;
 import de.tum.in.niedermr.ta.runner.configuration.property.templates.AbstractMultiClassnameProperty;
 
 public class MethodFiltersProperty extends AbstractMultiClassnameProperty<IMethodFilter> {
 
+	/** {@inheritDoc} */
 	@Override
 	public String getName() {
 		return "methodFilters";
 	}
 
-	@Override
-	protected String getDefault() {
-		return SetterGetterFilter.class.getName();
-	}
-
+	/** {@inheritDoc} */
 	@Override
 	public String getDescription() {
 		return "Method filters";
 	}
 
+	/** {@inheritDoc} */
 	@Override
 	protected Class<? extends IMethodFilter> getRequiredType() {
 		return IMethodFilter.class;
