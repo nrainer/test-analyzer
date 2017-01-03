3365c023d02a57f9fa7eb24b846d07bc85ddd3aa
nrainer <nrainer@users.noreply.github.com>
2017-01-03 17:29:09 +0100

    JavaConstants

--- test-analyzer-core/src/main/java/de/tum/in/niedermr/ta/core/code/constants/JavaConstants.java
+++ test-analyzer-core/src/main/java/de/tum/in/niedermr/ta/core/code/constants/JavaConstants.java
@@ -1,12 +1,23 @@
 package de.tum.in.niedermr.ta.core.code.constants;
 
+/** Java constants. */
 public interface JavaConstants {
+	/** {@value} */
 	public static final String PATH_SEPARATOR = "/";
+	/** {@value} */
 	public static final String PACKAGE_SEPARATOR = ".";
+	/** {@value} */
 	public static final String SUBCLASS_SEPARATOR = "$";
+	/** {@value} */
 	public static final String CLASS_METHOD_SEPARATOR = ".";
+	/** {@value} */
 	public static final String ARGUMENTS_SEPARATOR = ",";
+	/** {@value} */
 	public static final String ARGUMENTS_BEGIN = "(";
+	/** {@value} */
 	public static final String ARGUMENTS_END = ")";
+	/** {@value} */
 	public static final String RETURN_TYPE_SEPARATOR = ":";
+	/** {@value} */
+	public static final String ARRAY_BRACKETS = "[]";
 }
