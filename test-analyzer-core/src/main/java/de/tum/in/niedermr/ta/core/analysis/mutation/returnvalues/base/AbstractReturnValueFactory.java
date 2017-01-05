2077358981a5ac926b7b8b4381c3bd5d5c7349de
nrainer <nrainer@users.noreply.github.com>
2017-01-05 12:00:51 +0100

    AbstractReturnValueFactory: added type check

--- test-analyzer-core/src/main/java/de/tum/in/niedermr/ta/core/analysis/mutation/returnvalues/base/AbstractReturnValueFactory.java
+++ test-analyzer-core/src/main/java/de/tum/in/niedermr/ta/core/analysis/mutation/returnvalues/base/AbstractReturnValueFactory.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import de.tum.in.niedermr.ta.core.code.constants.JavaConstants;
 import de.tum.in.niedermr.ta.core.code.identifier.MethodIdentifier;
 
 public abstract class AbstractReturnValueFactory implements IReturnValueFactory {
@@ -23,11 +24,37 @@ public abstract class AbstractReturnValueFactory implements IReturnValueFactory 
 	@Override
 	public boolean supports(MethodIdentifier methodIdentifier, String returnType) {
 		try {
-			return createRecursiveWithException(methodIdentifier, returnType) != null;
+			Object instance = createRecursiveWithException(methodIdentifier, returnType);
+
+			if (instance == null) {
+				return false;
+			}
+
+			checkType(instance, returnType);
+			return true;
 		} catch (NoSuchElementException ex) {
 			return false;
 		} catch (Throwable t) {
 			return false;
+		}
+	}
+
+	protected void checkType(Object instance, String returnType) {
+		if (returnType.contains(JavaConstants.ARRAY_BRACKETS)) {
+			// ignore arrays
+			return;
+		}
+
+		try {
+			Class<?> cls = Class.forName(returnType);
+
+			if (!cls.isAssignableFrom(instance.getClass())) {
+				LOGGER.error(instance.getClass().getName() + " is not suitable for " + returnType);
+				throw new IllegalArgumentException();
+			}
+		} catch (ClassNotFoundException e) {
+			LOGGER.error("Failed to resolve the return type: " + returnType);
+			throw new IllegalArgumentException();
 		}
 	}
 
