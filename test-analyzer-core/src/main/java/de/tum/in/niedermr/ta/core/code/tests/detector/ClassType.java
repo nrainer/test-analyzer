d39306ca0079b9667c08b70b0483f43734a56cc1
nrainer <nrainer@users.noreply.github.com>
2017-01-07 10:34:39 +0100

    ClassType: added toString method

--- test-analyzer-core/src/main/java/de/tum/in/niedermr/ta/core/code/tests/detector/ClassType.java
+++ test-analyzer-core/src/main/java/de/tum/in/niedermr/ta/core/code/tests/detector/ClassType.java
@@ -4,22 +4,27 @@ package de.tum.in.niedermr.ta.core.code.tests.detector;
 public class ClassType {
 
 	/** Not a test class / application class */
-	public static final ClassType NO_TEST_CLASS = new ClassType(false, true);
+	public static final ClassType NO_TEST_CLASS = new ClassType("NO_TEST_CLASS", false, true);
 
 	/** Ignored test class (due to include / exclude patterns). */
-	public static final ClassType IGNORED_TEST_CLASS = new ClassType(false, false);
+	public static final ClassType IGNORED_TEST_CLASS = new ClassType("IGNORED_TEST_CLASS", false, false);
 
 	/** (Non-test) class in a test or an ignored class. */
-	public static final ClassType INNER_CLASS_IN_TEST_OR_IGNORED_CLASS = new ClassType(false, false);
+	public static final ClassType INNER_CLASS_IN_TEST_OR_IGNORED_CLASS = new ClassType(
+			"INNER_CLASS_IN_TEST_OR_IGNORED_CLASS", false, false);
 
 	/**
 	 * Abstract test class detected by a test class detector configured to
 	 * ignore these.
 	 */
-	public static final ClassType IGNORED_ABSTRACT_TEST_CLASS = new ClassType(false, false);
+	public static final ClassType IGNORED_ABSTRACT_TEST_CLASS = new ClassType("IGNORED_ABSTRACT_TEST_CLASS", false,
+			false);
 
 	/** Test class. */
-	public static final ClassType TEST_CLASS = new ClassType(true, false);
+	public static final ClassType TEST_CLASS = new ClassType("TEST_CLASS", true, false);
+
+	/** Identifier. */
+	private final String m_identifier;
 
 	/** Is considered as a test class. */
 	private final boolean m_isTestClass;
@@ -28,7 +33,8 @@ public class ClassType {
 	private final boolean m_isSourceClass;
 
 	/** Constructor. */
-	protected ClassType(boolean isTestClass, boolean isSourceClass) {
+	protected ClassType(String identifier, boolean isTestClass, boolean isSourceClass) {
+		m_identifier = identifier;
 		m_isTestClass = isTestClass;
 		m_isSourceClass = isSourceClass;
 	}
@@ -41,5 +47,11 @@ public class ClassType {
 	/** {@link #m_isSourceClass} */
 	public boolean isSourceClass() {
 		return m_isSourceClass;
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public String toString() {
+		return m_identifier;
 	}
 }
