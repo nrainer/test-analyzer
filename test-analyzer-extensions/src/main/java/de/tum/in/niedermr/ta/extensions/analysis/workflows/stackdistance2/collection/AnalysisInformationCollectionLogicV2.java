7c4ca77bbcf540cac113a2b1f45b333c7f63841f
nrainer <nrainer@users.noreply.github.com>
2017-01-15 23:53:49 +0100

    AnalysisInformationCollectionLogicV2: added logging

--- test-analyzer-extensions/src/main/java/de/tum/in/niedermr/ta/extensions/analysis/workflows/stackdistance2/collection/AnalysisInformationCollectionLogicV2.java
+++ test-analyzer-extensions/src/main/java/de/tum/in/niedermr/ta/extensions/analysis/workflows/stackdistance2/collection/AnalysisInformationCollectionLogicV2.java
@@ -3,6 +3,9 @@ package de.tum.in.niedermr.ta.extensions.analysis.workflows.stackdistance2.colle
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import de.tum.in.niedermr.ta.core.code.identifier.TestcaseIdentifier;
 import de.tum.in.niedermr.ta.extensions.analysis.workflows.stackdistance.logic.collection.AbstractAnalysisInformationCollectionLogic;
 import de.tum.in.niedermr.ta.extensions.analysis.workflows.stackdistance2.StackLogRecorderV2;
@@ -14,6 +17,9 @@ import de.tum.in.niedermr.ta.extensions.threads.ThreadNotifier;
  * Parameterless constructor required.
  */
 public class AnalysisInformationCollectionLogicV2 extends AbstractAnalysisInformationCollectionLogic {
+
+	/** Logger. */
+	private static final Logger LOGGER = LogManager.getLogger(AnalysisInformationCollectionLogicV2.class);
 
 	/** Prefixes of class names that should not be counted when computing the stack distance. */
 	private static final String[] STACK_COUNT_IGNORE_CLASS_NAME_PREFIXES = new String[] { "org.junit.",
@@ -28,7 +34,9 @@ public class AnalysisInformationCollectionLogicV2 extends AbstractAnalysisInform
 		stackManager.setStackCountIgnoreClassNamesPrefixes(STACK_COUNT_IGNORE_CLASS_NAME_PREFIXES);
 
 		ThreadNotifier.INSTANCE.registerListener(stackManager);
+		LOGGER.info("Registered StackManager at the ThreadNotifier.");
 		StackLogRecorderV2.setThreadStackManagerAndVerify(stackManager);
+		LOGGER.info("ThreadStackManager ist set and verified.");
 	}
 
 	/** {@inheritDoc} */
