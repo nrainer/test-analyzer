550c44debc464a655b58fd4edcbb2a408f047b3c
nrainer <nrainer@users.noreply.github.com>
2017-02-09 20:20:04 +0100

    IntegrationTest8: execute the test if the assumption is satisfied

--- test-analyzer-test-int/src/test/java/de/tum/in/niedermr/ta/test/integration/IntegrationTest8.java
+++ test-analyzer-test-int/src/test/java/de/tum/in/niedermr/ta/test/integration/IntegrationTest8.java
@@ -3,19 +3,22 @@ package de.tum.in.niedermr.ta.test.integration;
 import java.io.File;
 import java.io.IOException;
 
-import org.junit.Ignore;
+import org.junit.Assume;
 
 import de.tum.in.niedermr.ta.extensions.analysis.workflows.ExtensionEnvironmentConstants;
+import de.tum.in.niedermr.ta.extensions.threads.IModifiedThreadClass;
 import de.tum.in.niedermr.ta.runner.configuration.exceptions.ConfigurationException;
 
 /**
  * Integration test with extensions involved.<br/>
  * <li>Stack-analysis workflow V2</li>
  * <li>Stack-analysis workflow V3</li>
+ *
+ * <br/>
+ * Note that this test can only be run successfully if java.lang.Thread is replaced.
  * 
  * @see "configuration file in test data"
  */
-@Ignore("This test is ignored because it runs only successfully if java.lang.Thread is replaced")
 public class IntegrationTest8 extends AbstractIntegrationTest {
 
 	/** Expected stack distance V2 file. */
@@ -30,6 +33,9 @@ public class IntegrationTest8 extends AbstractIntegrationTest {
 	/** {@inheritDoc} */
 	@Override
 	public void executeTestLogic() throws ConfigurationException, IOException {
+		Assume.assumeTrue("This test requires a modified java.lang.Thread class",
+				IModifiedThreadClass.class.isAssignableFrom(Thread.class));
+
 		initializeFiles();
 
 		assertFilesExists(MSG_PATH_TO_TEST_JAR_IS_INCORRECT, new File(getCommonFolderTestData() + JAR_TEST_DATA));
