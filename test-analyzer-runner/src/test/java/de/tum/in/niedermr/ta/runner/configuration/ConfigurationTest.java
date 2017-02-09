8e2ad51604e4da32aed1016727af602b5978c3e1
nrainer <nrainer@users.noreply.github.com>
2017-02-09 19:39:12 +0100

    Configuration: add test case

--- test-analyzer-runner/src/test/java/de/tum/in/niedermr/ta/runner/configuration/ConfigurationTest.java
+++ test-analyzer-runner/src/test/java/de/tum/in/niedermr/ta/runner/configuration/ConfigurationTest.java
@@ -1,13 +1,19 @@
 package de.tum.in.niedermr.ta.runner.configuration;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 import de.tum.in.niedermr.ta.runner.configuration.exceptions.ConfigurationException;
+import de.tum.in.niedermr.ta.runner.configuration.extension.DynamicConfigurationKey;
+import de.tum.in.niedermr.ta.runner.configuration.extension.DynamicConfigurationKeyNamespace;
 import de.tum.in.niedermr.ta.runner.configuration.property.TestingTimeoutAbsoluteMaxProperty;
 
+/** Test {@link Configuration}. */
 public class ConfigurationTest {
+
+	/** Test. */
 	@Test(expected = ConfigurationException.class)
 	public void testInvalidConfig() throws ConfigurationException {
 		Configuration configuration = new Configuration();
@@ -17,6 +23,7 @@ public class ConfigurationTest {
 		configuration.validate();
 	}
 
+	/** Test. */
 	@Test
 	public void testTestingTimeout() {
 		Configuration configuration = new Configuration();
@@ -54,5 +61,20 @@ public class ConfigurationTest {
 		configuration.getTestingTimeoutPerTestcase().setValue(5);
 		configuration.getTestingTimeoutAbsoluteMax().setValue(20);
 		assertEquals(20, configuration.computeTestingTimeout(5));
+	}
+
+	/** Test. */
+	@Test
+	public void testToMultiLineString() {
+		Configuration configuration = new Configuration();
+		configuration.getNumberOfThreads().setValue(4);
+
+		DynamicConfigurationKey configurationKey = DynamicConfigurationKey
+				.create(DynamicConfigurationKeyNamespace.EXTENSION, "algorithm", "F1");
+		configuration.getDynamicValues().setRawValue(configurationKey, "F4");
+
+		String multiLineString = configuration.toMultiLineString();
+		assertTrue(multiLineString.contains("[numberOfThreads=1]"));
+		assertTrue(multiLineString.contains("extension.algorithm=F4"));
 	}
 }
