6b24e0c7d6b64471059c3b6f8c0073d780fcfe99
nrainer <nrainer@users.noreply.github.com>
2017-03-17 10:08:25 +0100

    Extend TestcaseIdentifier

--- test-analyzer-core/src/test/java/de/tum/in/niedermr/ta/core/code/identifier/TestcaseIdentifierTest.java
+++ test-analyzer-core/src/test/java/de/tum/in/niedermr/ta/core/code/identifier/TestcaseIdentifierTest.java
@@ -5,17 +5,21 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import de.tum.in.niedermr.ta.core.code.constants.BytecodeConstants;
+import de.tum.in.niedermr.ta.core.code.constants.JavaConstants;
 
+/** Test {@link TestcaseIdentifier}. */
 public class TestcaseIdentifierTest {
 	private static final String TEST_CASE_NAME = "testX";
 
 	private static final Class<?> EXPECTED_TEST_IDENTIFIER_CLASS = TestcaseIdentifierTest.class;
 	private static final String EXPECTED_TEST_IDENTIFIER_CLASS_NAME = EXPECTED_TEST_IDENTIFIER_CLASS.getName();
-	private static final String EXPECTED_TEST_IDENTIFIER_STRING = EXPECTED_TEST_IDENTIFIER_CLASS_NAME + TestcaseIdentifier.SEPARATOR + TEST_CASE_NAME;
+	private static final String EXPECTED_TEST_IDENTIFIER_STRING = EXPECTED_TEST_IDENTIFIER_CLASS_NAME
+			+ TestcaseIdentifier.SEPARATOR + TEST_CASE_NAME;
 
+	/** Test. */
 	@Test
 	public void testCreate() throws ClassNotFoundException {
-		TestcaseIdentifier testIdentifier = TestcaseIdentifier.create(TestcaseIdentifierTest.class, TEST_CASE_NAME);
+		TestcaseIdentifier testIdentifier = TestcaseIdentifier.create(EXPECTED_TEST_IDENTIFIER_CLASS, TEST_CASE_NAME);
 
 		assertEquals(EXPECTED_TEST_IDENTIFIER_CLASS, testIdentifier.resolveTestClass());
 		assertEquals(EXPECTED_TEST_IDENTIFIER_CLASS_NAME, testIdentifier.getTestClassName());
@@ -23,6 +27,16 @@ public class TestcaseIdentifierTest {
 		assertEquals(EXPECTED_TEST_IDENTIFIER_STRING, testIdentifier.get());
 	}
 
+	/** Test. */
+	@Test
+	public void testCreateFromJavaName() {
+		TestcaseIdentifier testIdentifier = TestcaseIdentifier.createFromJavaName(
+				EXPECTED_TEST_IDENTIFIER_CLASS_NAME + JavaConstants.CLASS_METHOD_SEPARATOR + TEST_CASE_NAME);
+		assertEquals(EXPECTED_TEST_IDENTIFIER_CLASS_NAME, testIdentifier.getTestClassName());
+		assertEquals(TEST_CASE_NAME, testIdentifier.getTestcaseName());
+	}
+
+	/** Test. */
 	@Test
 	public void testParse() throws ClassNotFoundException {
 		TestcaseIdentifier testIdentifier = TestcaseIdentifier.create(TestcaseIdentifierTest.class, TEST_CASE_NAME);
@@ -30,6 +44,7 @@ public class TestcaseIdentifierTest {
 		assertEquals(testIdentifier, TestcaseIdentifier.parse(testIdentifier.get()));
 	}
 
+	/** Test. */
 	@Test
 	public void testToMethodIdentifier() throws ClassNotFoundException {
 		TestcaseIdentifier testIdentifier = TestcaseIdentifier.create(TestcaseIdentifierTest.class, TEST_CASE_NAME);
