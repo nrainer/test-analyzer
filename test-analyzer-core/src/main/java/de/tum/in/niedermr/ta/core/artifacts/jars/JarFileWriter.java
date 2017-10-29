56e4d36a651f76005865f0c1a139439e8439d798
nrainer <nrainer@users.noreply.github.com>
2017-10-29 20:36:54 +0100

    Fix

--- test-analyzer-core/src/main/java/de/tum/in/niedermr/ta/core/artifacts/jars/JarFileWriter.java
+++ test-analyzer-core/src/main/java/de/tum/in/niedermr/ta/core/artifacts/jars/JarFileWriter.java
@@ -6,6 +6,7 @@ import java.util.jar.JarEntry;
 import java.util.jar.JarOutputStream;
 
 import de.tum.in.niedermr.ta.core.artifacts.io.AbstractArtifactOutputWriter;
+import de.tum.in.niedermr.ta.core.common.constants.FileSystemConstants;
 
 class JarFileWriter extends AbstractArtifactOutputWriter {
 	private JarOutputStream m_outputStream;
@@ -25,11 +26,13 @@ class JarFileWriter extends AbstractArtifactOutputWriter {
 
 	/** {@inheritDoc} */
 	@Override
-	protected void writeElement(String entryName, byte[] data) throws IOException {
+	protected void writeElement(String originalEntryName, byte[] data) throws IOException {
 		if (m_outputStream == null) {
 			m_outputStream = new JarOutputStream(new FileOutputStream(getArtifactPath()));
 		}
 
+		String entryName = originalEntryName.replace(FileSystemConstants.PATH_SEPARATOR_ALTERNATIVE,
+				FileSystemConstants.PATH_SEPARATOR);
 		JarEntry entry = new JarEntry(entryName);
 		m_outputStream.putNextEntry(entry);
 		m_outputStream.write(data);
