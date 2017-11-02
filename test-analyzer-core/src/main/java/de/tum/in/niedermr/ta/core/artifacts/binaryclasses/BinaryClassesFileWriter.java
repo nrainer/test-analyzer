02595d77be643b4f9ee677e41db29c04fa5b625d
nrainer <nrainer@users.noreply.github.com>
2017-11-02 20:49:11 +0100

    Fix

--- test-analyzer-core/src/main/java/de/tum/in/niedermr/ta/core/artifacts/binaryclasses/BinaryClassesFileWriter.java
+++ test-analyzer-core/src/main/java/de/tum/in/niedermr/ta/core/artifacts/binaryclasses/BinaryClassesFileWriter.java
@@ -1,9 +1,14 @@
 package de.tum.in.niedermr.ta.core.artifacts.binaryclasses;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import org.conqat.lib.commons.filesystem.FileSystemUtils;
+
 import de.tum.in.niedermr.ta.core.artifacts.io.AbstractArtifactOutputWriter;
+import de.tum.in.niedermr.ta.core.common.constants.FileSystemConstants;
+import de.tum.in.niedermr.ta.core.common.util.FileUtility;
 
 class BinaryClassesFileWriter extends AbstractArtifactOutputWriter {
 	/** Constructor. */
@@ -20,7 +25,11 @@ class BinaryClassesFileWriter extends AbstractArtifactOutputWriter {
 	/** {@inheritDoc} */
 	@Override
 	protected void writeElement(String entryName, byte[] data) throws IOException {
-		FileOutputStream outputStream = new FileOutputStream(getArtifactPath());
+		String elementPath = FileUtility.ensurePathEndsWithPathSeparator(getArtifactPath(),
+				FileSystemConstants.PATH_SEPARATOR) + entryName;
+		FileSystemUtils.ensureParentDirectoryExists(new File(elementPath));
+
+		FileOutputStream outputStream = new FileOutputStream(elementPath);
 		outputStream.write(data);
 		outputStream.close();
 	}
