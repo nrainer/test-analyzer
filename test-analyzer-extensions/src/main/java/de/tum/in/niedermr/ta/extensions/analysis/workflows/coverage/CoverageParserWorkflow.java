0ac7049410a29355729cf5a76b5fec24cf6daa16
nrainer <nrainer@users.noreply.github.com>
2018-01-19 23:03:48 +0100

    Extract class

--- test-analyzer-extensions/src/main/java/de/tum/in/niedermr/ta/extensions/analysis/workflows/coverage/CoverageParserWorkflow.java
+++ test-analyzer-extensions/src/main/java/de/tum/in/niedermr/ta/extensions/analysis/workflows/coverage/CoverageParserWorkflow.java
@@ -1,42 +1,13 @@
 package de.tum.in.niedermr.ta.extensions.analysis.workflows.coverage;
 
 import de.tum.in.niedermr.ta.extensions.analysis.workflows.ExtensionEnvironmentConstants;
-import de.tum.in.niedermr.ta.extensions.analysis.workflows.converter.AbstractConverterWorkflow;
 import de.tum.in.niedermr.ta.extensions.analysis.workflows.coverage.steps.AggregatedCoverageParserStep;
-import de.tum.in.niedermr.ta.runner.configuration.extension.DynamicConfigurationKey;
-import de.tum.in.niedermr.ta.runner.configuration.extension.DynamicConfigurationKeyNamespace;
 
 /**
  * Parser for aggregated coverage information. Currently, only coverage in form
  * of XML from JaCoCo is supported.
  */
-public class CoverageParserWorkflow extends AbstractConverterWorkflow<AggregatedCoverageParserStep> {
-
-	/** Default name of the coverage file. */
-	private static final String DEFAULT_COVERAGE_FILE_NAME = "coverage.xml";
-
-	/**
-	 * <code>extension.code.coverage.useMultipleOutputFiles</code>: Split the output
-	 * into multiple files.
-	 */
-	public static final DynamicConfigurationKey CONFIGURATION_KEY_USE_MULTIPLE_OUTPUT_FILES = DynamicConfigurationKey
-			.create(DynamicConfigurationKeyNamespace.EXTENSION, "code.coverage.useMultipleOutputFiles", false);
-
-	/** <code>extension.code.coverage.file</code> */
-	public static final DynamicConfigurationKey CONFIGURATION_KEY_COVERAGE_FILE = DynamicConfigurationKey
-			.create(DynamicConfigurationKeyNamespace.EXTENSION, "code.coverage.file", DEFAULT_COVERAGE_FILE_NAME);
-
-	/** {@inheritDoc} */
-	@Override
-	protected DynamicConfigurationKey getConfigurationKeyForMultipleOutputFileUsage() {
-		return CONFIGURATION_KEY_USE_MULTIPLE_OUTPUT_FILES;
-	}
-
-	/** {@inheritDoc} */
-	@Override
-	protected DynamicConfigurationKey getConfigurationKeyForInputFile() {
-		return CONFIGURATION_KEY_COVERAGE_FILE;
-	}
+public class CoverageParserWorkflow extends AbstractCoverageParserWorkflow<AggregatedCoverageParserStep> {
 
 	/** {@inheritDoc} */
 	@Override
