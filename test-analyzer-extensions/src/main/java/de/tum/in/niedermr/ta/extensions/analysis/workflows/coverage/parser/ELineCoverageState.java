aab949fadb1e78e02b0d228b4ab03d3e81301da6
nrainer <nrainer@users.noreply.github.com>
2018-01-20 08:11:56 +0100

    LineLevelCoverageParserWorkflow

---
+++ test-analyzer-extensions/src/main/java/de/tum/in/niedermr/ta/extensions/analysis/workflows/coverage/parser/ELineCoverageState.java
@@ -0,0 +1,36 @@
+package de.tum.in.niedermr.ta.extensions.analysis.workflows.coverage.parser;
+
+/** Coverage state of a source code line. */
+public enum ELineCoverageState {
+
+	/** Not coverable. */
+	NOT_COVERABLE,
+	/** Not covered. */
+	NOT_COVERED,
+	/** Partially covered. */
+	PARTIALLY_COVERED,
+	/** Fully covered. */
+	FULLY_COVERED;
+
+	/** Get the state based on instruction counts. */
+	public static ELineCoverageState get(int countCoveredInstructions, int countMissedInstructions) {
+		if (countCoveredInstructions > 0 && countMissedInstructions == 0) {
+			return FULLY_COVERED;
+		}
+
+		if (countCoveredInstructions == 0 && countMissedInstructions > 0) {
+			return NOT_COVERED;
+		}
+
+		if (countCoveredInstructions > 0 && countMissedInstructions > 0) {
+			return PARTIALLY_COVERED;
+		}
+
+		return NOT_COVERABLE;
+	}
+
+	/** Get the name. */
+	public String getName() {
+		return toString();
+	}
+}
