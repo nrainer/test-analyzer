086f19b7004fd6d209a2f14ef47cf92cf6e78c10
nrainer <nrainer@users.noreply.github.com>
2018-01-20 11:23:56 +0100

    JaCoCoLineLevelParser: SQL schema adjustments

--- test-analyzer-extensions/sql/JaCoCoLineLevelParser/1_schema/1_import_sub_schema.sql
+++ test-analyzer-extensions/sql/JaCoCoLineLevelParser/1_schema/1_import_sub_schema.sql
@@ -6,7 +6,9 @@ CREATE TABLE Commit_Info
 (
 	commitId INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	projectKey VARCHAR(5) NOT NULL,
-	commitHash VARCHAR(40) NOT NULL
+	commitHash VARCHAR(40) NOT NULL,
+	commitHashShort VARCHAR(5) GENERATED ALWAYS AS (SUBSTRING(commitHash, 1, 5)) VIRTUAL,
+	isSourceFileNamesImported TINYINT(1) NOT NULL DEFAULT 0
 );
 
 CREATE TABLE Method_Location_Import
@@ -31,13 +33,13 @@ CREATE TABLE Line_Coverage_Import
 	sourceFileName VARCHAR(128) NOT NULL COLLATE UTF8_BIN,
 	lineNumber INT(6) NOT NULL,
     coverageState ENUM ('NOT_COVERABLE', 'NOT_COVERED', 'PARTIALLY_COVERED', 'FULLY_COVERED'),
-	fullPathHash VARCHAR(64) GENERATED ALWAYS AS (CONCAT(MD5(packageName), MD5(sourceFileName))) VIRTUAL
+	sourceFilePathHash VARCHAR(64) GENERATED ALWAYS AS (CONCAT(MD5(packageName), MD5(sourceFileName))) VIRTUAL
 );
 
 CREATE INDEX idx_ml_imp_1 ON Method_Location_Import(projectKey, commitHash);
 CREATE INDEX idx_ml_imp_2 ON Method_Location_Import(startLine);
 CREATE INDEX idx_ml_imp_3 ON Method_Location_Import(fullMethodHash);
 CREATE INDEX idx_lc_imp_1 ON Line_Coverage_Import(projectKey, commitHash, sessionNumber);
-CREATE INDEX idx_lc_imp_2 ON Line_Coverage_Import(fullPathHash);
+CREATE INDEX idx_lc_imp_2 ON Line_Coverage_Import(sourceFilePathHash);
 
 ALTER TABLE Commit_Info ADD CONSTRAINT uc_comm_info_1 UNIQUE (projectKey, commitHash);
--- test-analyzer-extensions/sql/JaCoCoLineLevelParser/1_schema/2_analysis_sub_schema.sql
+++ test-analyzer-extensions/sql/JaCoCoLineLevelParser/1_schema/2_analysis_sub_schema.sql
@@ -7,9 +7,10 @@ CREATE TABLE Source_File_Info
 	commitId INT(11) NOT NULL REFERENCES Commit_Info(commitId),
 	packageName VARCHAR(512) NOT NULL COLLATE UTF8_BIN,
 	sourceFileName VARCHAR(128) NOT NULL COLLATE UTF8_BIN,
-	fullPathHash VARCHAR(64) GENERATED ALWAYS AS (CONCAT(MD5(packageName), MD5(sourceFileName))) VIRTUAL
+	sourceFilePathHash VARCHAR(64) GENERATED ALWAYS AS (CONCAT(MD5(packageName), MD5(sourceFileName))) VIRTUAL
 );
 
+-- NOT_COVERABLE not allowed on purpose
 CREATE TABLE Line_Coverage_Info
 (
 	id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -17,10 +18,13 @@ CREATE TABLE Line_Coverage_Info
 	sessionNumber INT(2) NOT NULL,
 	sourceFileId INT(11) NOT NULL REFERENCES Source_File_Info(sourceFileId),
 	lineNumber INT(6) NOT NULL,
-    coverageState ENUM ('NOT_COVERABLE', 'NOT_COVERED', 'PARTIALLY_COVERED', 'FULLY_COVERED')
+    coverageState ENUM ('NOT_COVERED', 'PARTIALLY_COVERED', 'FULLY_COVERED'),
+    isFullyCovered TINYINT(1) GENERATED ALWAYS AS (coverageState = 'FULLY_COVERED') VIRTUAL,
+    isPartiallyCovered TINYINT(1) GENERATED ALWAYS AS (coverageState = 'PARTIALLY_COVERED') VIRTUAL,
+    isNotCovered TINYINT(1) GENERATED ALWAYS AS (coverageState = 'NOT_COVERED') VIRTUAL
 );
 
 CREATE INDEX idx_sf_info_1 ON Source_File_Info(commitId);
-CREATE INDEX idx_sf_info_2 ON Source_File_Info(fullPathHash);
+CREATE INDEX idx_sf_info_2 ON Source_File_Info(sourceFilePathHash);
 
 ALTER TABLE Line_Coverage_Info ADD CONSTRAINT uc_lc_info_1 UNIQUE (commitId, sessionNumber, sourceFileId, lineNumber);
