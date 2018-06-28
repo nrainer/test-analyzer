29e8d6c69f39c7acb6e0cd14d08873cd503129ae
nrainer <nrainer@users.noreply.github.com>
2018-06-28 15:38:58 +0200

    Documentation

--- test-analyzer-sdist/src/main/java/de/tum/in/niedermr/ta/extensions/threads/IModifiedThreadClass.java
+++ test-analyzer-sdist/src/main/java/de/tum/in/niedermr/ta/extensions/threads/IModifiedThreadClass.java
@@ -1,6 +1,10 @@
 package de.tum.in.niedermr.ta.extensions.threads;
 
-/** Interface to check if the modified <code>java.lang.Thread</code> class is loaded. */
+/**
+ * Interface to check if the modified <code>java.lang.Thread</code> class is loaded.
+ * 
+ * @see "thread-class-modification-java_1.8.0_60.patch"
+ */
 public interface IModifiedThreadClass {
 	// NOP
 }
